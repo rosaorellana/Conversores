fc894a23ccb2e6c927f2c2e626ac5b897ae6eeb5
DELL <valenciaquiroz.01@gmail.com>
2020-09-08 14:12:57 -0600

    Segundo avance conversores

--- app/src/main/java/com/example/programacionlll/Masa.java
+++ app/src/main/java/com/example/programacionlll/Masa.java
@@ -3,6 +3,10 @@ package com.example.programacionlll;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class Masa extends AppCompatActivity {
 
@@ -10,5 +14,27 @@ public class Masa extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_masa);
+    }   Button button =(Button)findViewById(R.id.idconvertir);
+
+    public void masa(View v){
+        try{
+            TextView tempVal=(TextView)findViewById(R.id.edtCantidad);
+            Double cantidad=Double.parseDouble(tempVal.getText().toString());
+
+            Spinner spn=(Spinner)findViewById(R.id.cboMasa);
+            Integer de =spn.getSelectedItemPosition();
+
+            Spinner spna=(Spinner)findViewById(R.id.cboMasas);
+            Integer a =spna.getSelectedItemPosition();
+            Double []pesos={0.4536,453.6,16.00,45359.27,453592.37,4535.92,4.5359,45.3592,0.04,1.00};
+
+            Double valor2=pesos[a]/pesos[de]*cantidad;
+            tempVal=(TextView)findViewById(R.id.txtRespuesta);
+            tempVal.setText("Respuesta:"+valor2);
+        }catch(Exception error){
+            TextView tempVal = (TextView) findViewById(R.id.txtRespuesta);
+            tempVal.setText("Ingrese la Cantidad a Convertir ");
+        }
+
     }
 }
--- app/src/main/java/com/example/programacionlll/Transdatos.java
+++ app/src/main/java/com/example/programacionlll/Transdatos.java
@@ -3,6 +3,10 @@ package com.example.programacionlll;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class Transdatos extends AppCompatActivity {
 
@@ -10,5 +14,28 @@ public class Transdatos extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transdatos);
+    }   Button button = (Button)findViewById(R.id.idconvertir);
+
+    public void transferencia(View v){
+        try{
+            TextView tempVal=(TextView)findViewById(R.id.edtCantidad);
+            Double cantidad3=Double.parseDouble(tempVal.getText().toString());
+
+            Spinner spn=(Spinner)findViewById(R.id.cboDato);
+            Integer de =spn.getSelectedItemPosition();
+
+            Spinner spna=(Spinner)findViewById(R.id.cboDatos);
+            Integer a =spna.getSelectedItemPosition();
+
+            Double[]unidades={1e-9,1e-6,0.000125,1e-12};
+
+            Double valor4=unidades[a]/unidades[de]*cantidad3;
+            tempVal=(TextView)findViewById(R.id.txtRespuesta);
+            tempVal.setText("Respuesta:"+valor4);
+        }catch(Exception error){
+            TextView tempVal = (TextView) findViewById(R.id.txtRespuesta);
+            tempVal.setText("Ingrese la Cantidad a Convertir ");
+        }
+
     }
 }
